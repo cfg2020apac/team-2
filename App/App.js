62a2a27694131ed81c8aa5a54170691bb4dbf154
LinHuiqing <lin.hui.qing1999@gmail.com>
2020-10-09 17:56:33 +0800

    add firebase cred to code

--- App/App.js
+++ App/App.js
@@ -7,14 +7,13 @@ require('dotenv').config();
 export default class App extends Component {
   componentWillMount() {
     const firebaseConfig = {
-      apiKey: process.env.FB_API_KEY,
-      authDomain: process.env.FB_AUTH_DOMAIN,
-      databaseURL: process.env.FB_DATABASE_URL,
-      projectId: process.env.FB_PROJECT_ID,
-      storageBucket: process.env.FB_STORAGE_BUCKET,
-      messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
-      appId: process.env.FB_APP_ID,
-      measurementId: process.env.FB_MEASUREMENT_ID
+      apiKey: "AIzaSyCLBaMaq2iCIw-0qMPnAQjM-1lOJWcrSKE",
+      authDomain: "nus-home.firebaseapp.com",
+      databaseURL: "https://nus-home.firebaseio.com",
+      projectId: "nus-home",
+      storageBucket: "",
+      messagingSenderId: "489089414375",
+      appId: "1:489089414375:web:434d7d3e3d64bc94"
     };
     if (!firebase.apps.length) {
       firebase.initializeApp(firebaseConfig);
